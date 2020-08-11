578f110d2b31da246cea9113d1ec55dfa1c7e1d5
SteinsHead <1310605444@qq.com>
2020-08-11 14:34:29 +0800

    initial commit

---
+++ FaceId/src/main/java/com/example/FaceId/FaceIdApplication.java
@@ -0,0 +1,14 @@
+package com.example.FaceId;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceIdApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hh");
+		SpringApplication.run(FaceIdApplication.class, args);
+	}
+
+}
